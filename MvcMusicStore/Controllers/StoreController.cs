258f75672e070afe14ec4ef9084498259d2d0c8b
Steve LeVesconte <SteveLeVesconte@Gmail.com>
2025-11-02 06:08:43 -0800

    Added Business Models - in Part 4

--- MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -3,21 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcMusicStore.Models;
 
 namespace MvcMusicStore.Controllers
 {
     public class StoreController : Controller
     {
-        public string Index()
+        public ActionResult Index()
         {
-            return "Hello from Store.Index()";
+            var genres = new List<Genre>
+    {
+        new Genre { Name = "Disco"},
+        new Genre { Name = "Jazz"},
+        new Genre { Name = "Rock"}
+    };
+            return View(genres);
         }
 
-        // GET: Store
-        //public ActionResult Index()
-        //{
-        //    return View();
-        //}
 
         public string Browse(string genre)
         {
@@ -26,11 +28,11 @@ namespace MvcMusicStore.Controllers
             return message;
         }
 
-        public string Details(int id)
+        // GET: /Store/Details/5
+        public ActionResult Details(int id)
         {
-            string message = "Store.Details, ID = " + id;
-
-            return message;
+            var album = new Album { Title = "Album " + id };
+            return View(album);
         }
     }
 }
